334b6986e9062c90afd565ccafbfda44401e5bab
pietje <goed0058@hz.nl>
2023-01-19 11:21:45 +0100

    public folder with image and html

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-  res.send(home.html)
+  res.sendFile('index.html', {root: "public"})
 })
 
 app.listen(port, () => {
